52e6504c2344313674bd536f72ca2d52a0629301
Andres Espina Duarte <36141101+AndresEspinaDuarte@users.noreply.github.com>
2024-12-01 03:11:32 -0300

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  apiUrl:"https://uber-nodejs-server-git-d61f89-guillermovillacuratorres-projects.vercel.app/api/", //uber.matiivilla.cl
+  firebaseConfig: {
+    apiKey: "AIzaSyDYYS9p9OMjY_M537c69m1btvbt8HrljQA",
+    authDomain: "proyectoduoc-bfe8d.firebaseapp.com",
+    projectId: "proyectoduoc-bfe8d",
+    storageBucket: "proyectoduoc-bfe8d.appspot.com",
+    messagingSenderId: "179995081812",
+    appId: "1:179995081812:web:f2390c2279b80b5196c1c1",
+    measurementId: "G-NE6ED65MT6"
+  } //uber.matiivilla.cl
+};
